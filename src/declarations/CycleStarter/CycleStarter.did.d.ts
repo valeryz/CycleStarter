30a33c5f9be31bfeb4b79ce4ff8d05a4ceee6289
Valeriy Zamaraiev <valeriy.zamaraiev@dfinity.org>
2021-09-07 11:01:43 +0200

    start adding new methods

--- src/declarations/CycleStarter/CycleStarter.did.d.ts
+++ src/declarations/CycleStarter/CycleStarter.did.d.ts
@@ -6,7 +6,11 @@ export interface Project {
   'name' : string,
   'description' : string,
 }
+export type Result = { 'ok' : null } |
+  { 'err' : string };
 export interface _SERVICE {
+  'claimDonatedCycles' : (arg_0: string, arg_1: bigint) => Promise<Result>,
+  'donateCyclesToProject' : (arg_0: string, arg_1: bigint) => Promise<Result>,
   'getICPBalance' : () => Promise<bigint>,
   'getProjects' : () => Promise<Array<Project>>,
 }
--- src/declarations/CycleStarter/CycleStarter.did.js
+++ src/declarations/CycleStarter/CycleStarter.did.js
@@ -1,4 +1,5 @@
 export const idlFactory = ({ IDL }) => {
+  const Result = IDL.Variant({ 'ok' : IDL.Null, 'err' : IDL.Text });
   const Project = IDL.Record({
     'id' : IDL.Nat,
     'imgUrl' : IDL.Text,
@@ -7,6 +8,8 @@ export const idlFactory = ({ IDL }) => {
     'description' : IDL.Text,
   });
   return IDL.Service({
+    'claimDonatedCycles' : IDL.Func([IDL.Text, IDL.Nat], [Result], []),
+    'donateCyclesToProject' : IDL.Func([IDL.Text, IDL.Nat], [Result], []),
     'getICPBalance' : IDL.Func([], [IDL.Int], ['query']),
     'getProjects' : IDL.Func([], [IDL.Vec(Project)], ['query']),
   });
